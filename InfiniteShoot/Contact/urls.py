e5aeaa1e4716b20b74f5b8b7977ff84a583a5229
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-16 22:01:38 +0200

    Update InfiniteShoot

--- InfiniteShoot/Contact/urls.py
+++ InfiniteShoot/Contact/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contact/', views.contact_view, name='contact_view'),
-    path('newsletter/', views.newsletter_view, name='newsletter_view'),
+    path('contact/', views.ContactView.as_view(), name='contact_view'),
+    path('newsletter/', views.NewsletterView.as_view(), name='newsletter_view'),
 ]
 
--- InfiniteShoot/Gallery/admin.py
+++ InfiniteShoot/Gallery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ImagesClient, PlatformPresentationImage
 
-# Register your models here.
+
+class PlatformPresentationImageAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class ImagesClientAdmin(admin.ModelAdmin):
+    list_display = ('client', 'name')
+
+
+admin.site.register(PlatformPresentationImage, PlatformPresentationImageAdmin)
+admin.site.register(ImagesClient, ImagesClientAdmin)
+
